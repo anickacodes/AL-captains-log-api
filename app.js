b15f3dca04b6bdf2e156438c60028d0a4d4d6c84
anickacodes <anickalewis@pursuit.org>
2023-10-18 04:23:55 -0400

    successful post , get, delete

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const logs = require("./models/log.js");
 const app = express()
 //parse incoming middleware
 app.use(express.json()) 
-// app.use()
 
 
 app.get('/', (req, res) => {
@@ -22,6 +21,54 @@ app.get('/logs/:arrayIndex', (req, res) => {
     } else 
     res.redirect( 'https://github.com/joinpursuit/captains-log-api' )
 })
+
+app.post('/logs', (req, res) => {
+    const { captainName,
+        title,
+        post,
+        mistakesWereMadeToday,
+        daysSinceLastCrisis
+    } = req.body;
+    const newLog = req.body
+    console.log(`sent, ${JSON.stringify(newLog)}`);
+    logs.push(newLog)
+    res.status(201).json(newLog)
+    
+})
+
+app.delete('/logs/:id', (req, res) => {
+    const {id} = req.params
+    logs.splice(id, 1)
+    res.send('Ok')
+})
+
+
+
+const alphaOrder = (req, res, next) => {
+console.log('✅')
+
+
+if (req.body.hasOwnProperty('name')){
+    return next();
+} else {
+    res.send('uh oh')
+}
+
+}
+
+app.post('/', alphaOrder, (req, res) => {
+    const sortedLog = Object.keys(logs).sort()
+    console.log('sent')
+    logs.push(sortedLog)
+    res.send('Ok')
+})
+
+
+
+app.get('/logs?order=asc', (req, res) => {
+
+})
+
 
 
 app.all('*', (req, res) => { 
